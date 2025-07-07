4d46b54c9dced99977f0639bef93e4db157783ff
Abbos Uzoqov <97328979+AbbosUzoqov@users.noreply.github.com>
2025-07-07 17:25:24 +0500

    Update auto_test_post_processor.py

--- tests/auto_test_post_processor.py
+++ tests/auto_test_post_processor.py
@@ -1,34 +1,18 @@
 import pytest
-from types import SimpleNamespace
-import torch
+from handlers.models import Box, Detection, Detections
 from handlers.post_processor import PostProcessor
-from handlers.models import Detections
-
-class MockBox:
-    def __init__(self, xyxy, conf, cls):
-        self.xyxy = torch.tensor([xyxy])  
-        self.conf = torch.tensor([conf])
-        self.cls = torch.tensor([cls])
-
-class MockResult:
-    def __init__(self, boxes, names):
-        self.boxes = boxes
-        self.names = names
 
 def test_postprocessor_handle():
-    # Подготавливаем mock-данные
-    boxes = [
-        MockBox([0, 0, 10, 10], conf=0.9, cls=0),
-        MockBox([20, 20, 40, 40], conf=0.85, cls=2)
-    ]
-    result = MockResult(boxes=boxes, names={0: 'person', 2: 'car'})
+    det1 = Detection(box=Box(0, 0, 10, 10), score=0.9, label_as_int=1, label_as_str='person')
+    det2 = Detection(box=Box(10, 10, 20, 20), score=0.8, label_as_int=2, label_as_str='car')
+    detections_list = [det1, det2]
 
     processor = PostProcessor()
     processor.on_start()
-    detections = processor.handle([result])
+    result = processor.handle(detections_list)
     processor.on_exit()
 
-    assert isinstance(detections, Detections)
-    assert len(detections.detections) == 2
-    assert detections.detections[0].label_as_str == 'person'
-    assert detections.detections[1].label_as_str == 'car'
+    assert isinstance(result, Detections)
+    assert len(result.detections) == 2
+    assert result.detections[0].label_as_str == 'person'
+    assert result.detections[1].label_as_str == 'car'
